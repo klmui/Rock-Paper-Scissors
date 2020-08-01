73dc73d314d713ea4cf5b4997da3c2fd2079efbd
Kenneth Mui <klmui@wisc.edu>
2020-08-01 17:41:46 -0500

    first commit

---
+++ app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rock_paper_scissors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
